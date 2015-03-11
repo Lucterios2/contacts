119b21e29b1cac1c65a8a2d5228dd9bd46cd726d
Laurent GAY <l.gay@sd-libre.fr>
2015-03-11 14:09:23 +0100

    contacts init

---
+++ lucterios/contacts/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+'''
+Created on march 2015
+
+@author: sd-libre
+'''
+
+from __future__ import unicode_literals
+from os.path import dirname, join, isfile
+
+def get_build():
+    file_name = join(dirname(__file__), 'build')
+    if isfile(file_name):
+        with open(file_name) as flb:
+            return flb.read()
+    return "0"
+
+__version__ = "2.0b" + get_build()
+
+def __title__():
+    from django.utils.translation import ugettext_lazy as _
+    return _("Lucterios contacts")
---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+'''
+Created on fevr. 2015
+
+@author: sd-libre
+'''
+
+from distutils.core import setup
+from lucterios.contacts import __version__
+
+setup(
+    name="Lucterios_contacts",
+    version=__version__,
+    author="Lucterios",
+    author_email="support@lucterios.org",
+    url="http://www.lucterios.org",
+    description="contacts managment module for Lucterios.",
+    long_description="""
+    contacts managment module for Lucterios.
+    """,
+    include_package_data=True,
+    platforms=('Any',),
+    license="GNU General Public License v2",
+    # Packages
+    packages=["lucterios", "lucterios.contacts"],
+    package_data={
+       "lucterios.contacts":['build', 'images/*', 'locale/*/*/*', 'help/*'],
+    },
+    install_requires=["Lucterios>=2.0"],
+)
